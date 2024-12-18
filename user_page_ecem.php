99c2bc3d75cb44941dfa42845a948e3e34e1d31d
Ecem Çakallı <66572639+ewok0116@users.noreply.github.com>
2024-12-18 03:11:23 +0300

    Create user_page_ecem.php

---
+++ user_page_ecem.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h1 style='text-align: center; margin-top: 20px;'>Welcome, User!</h1>";
+echo "<p style='text-align: center;'>This is the user dashboard. Add user-specific content here.</p>";
+?>
